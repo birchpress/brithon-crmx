4e979158da6e835c3c2aff390b9a97a36d23c013
Ray Xue <ray@brithon.com>
2015-11-19 19:39:49 +0800

    Use builder-v1.4.0.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,11 @@
 'use strict';
 
+const source = require('vinyl-source-stream');
+const es = require('event-stream');
+const globby = require('globby');
+const browserify = require('browserify');
+const logger = require('gulp-logger');
+const rename = require('gulp-rename');
 const gulp = require('gulp');
 
 const builder = require('birchpress-builder')(gulp);
@@ -11,18 +17,35 @@ const productConfig = {
     'brithon-crmx.php',
     'loader.php',
     'package.php'
-  ],
+  ]
 
-  corePublishSrc: [
-    'screenshots/**/*',
-    'readme.txt'
-  ],
-
-  shouldBrowserify: true,
-
-  coreBrowserifyDirs: [],
-
-  coreBrowserifyRecursiveDirs: ['modules/**/assets/js/apps/']
 };
 
 builder.setProductConfig(productConfig);
+
+const bundleFiles = ['modules/**/assets/js/apps/**/index.js'];
+
+gulp.task('bundle', function() {
+  const tasks = globby.sync(bundleFiles).map(indexjs => {
+    return browserify(indexjs)
+      .transform('babelify', {
+        presets: ['react']
+      })
+      .transform('browserify-shim', {
+        global: true
+      })
+      .transform('pkgify')
+      .bundle()
+      .pipe(source(indexjs))
+      .pipe(logger({
+        extname: '.bundle.js',
+        showChange: true
+      }))
+      .pipe(rename({
+        extname: '.bundle.js'
+      }))
+      .pipe(gulp.dest('./'));
+  });
+
+  return es.merge(...tasks);
+});
